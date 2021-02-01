44fb55b1b239eeb56d904b543b31dec283a12e8b
Zohaib <khaliqzohaib92@gmail.com>
2021-01-31 22:13:33 -0500

    Add chart and transactions sample view
    - Add chart sample view
    - Create new transaction class to make things more managable
    - Add sample transations to build the view transation.

--- Expense_Planner/lib/main.dart
+++ Expense_Planner/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:Expense_Planner/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -13,14 +14,39 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transations = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'Weekly Groceries',
+        amount: 16.53,
+        date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
-        child: Text('Widget Playground!'),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('CHART!'),
+              elevation: 5,
+            ),
+          ),
+          Column(
+            children: transations.map((tx) {
+              return Card(
+                child: Text(tx.title),
+              );
+            }).toList(),
+          )
+        ],
       ),
     );
   }
