14f4fca575b3e6208db006af4d6db55b1d7bd8f1
Zohaib <khaliqzohaib92@gmail.com>
2021-02-06 19:35:48 -0500

    Fix the issue of clearing the input when tab other field

--- Expense_Planner/lib/widgets/new_transaction.dart
+++ Expense_Planner/lib/widgets/new_transaction.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
-  final titleController =
-      TextEditingController(); // solution to get text field text on submit click in a stateless widget
-  final amountController = TextEditingController();
+class NewTransaction extends StatefulWidget {
   final Function addNewTransaction;
 
   NewTransaction(this.addNewTransaction);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   void submitData() {
     final eneteredTitle = titleController.text;
@@ -16,7 +21,7 @@ class NewTransaction extends StatelessWidget {
       return;
     }
 
-    addNewTransaction(eneteredTitle, enteredAmount);
+    widget.addNewTransaction(eneteredTitle, enteredAmount);
   }
 
   @override
