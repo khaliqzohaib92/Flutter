9e07ec736c80067e693680e919a6c1794952eccd
Zohaib <khaliqzohaib92@gmail.com>
2021-02-27 00:14:51 -0500

    Add Chart
    * Add chart widget
    * Map chart to show transaction inserted
    * Allow char to show no data if there are no transactions

--- Expense_Planner/lib/main.dart
+++ Expense_Planner/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:Expense_Planner/widgets/chart.dart';
 import 'package:Expense_Planner/widgets/new_transaction.dart';
 import 'package:Expense_Planner/widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
@@ -67,6 +68,12 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
+    }).toList();
+  }
+
   void _startAddNewTransaction(BuildContext ctx) {
     showModalBottomSheet(
         context: ctx,
@@ -93,13 +100,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-              width: double.infinity,
-              child: Card(
-                child: Text('CHART!'),
-                elevation: 5,
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_userTransactions),
           ],
         ),
